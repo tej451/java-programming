9b350179ac22240911c782c362d0c67a8117418f
Kollipara Tejkumar <Kollipara_Tejkumar@gap.com>
2017-11-18 12:36:33 +0530

    JAVA-000[Tej]: Adding base project

---
+++ src/com/core/oops/ExecutionOrder.java
@@ -0,0 +1,35 @@
+package com.core.oops;
+
+public class ExecutionOrder {
+	public static void main(String[] args) {
+		new Child();
+	}
+
+}
+
+class Parent {
+	public Parent() {
+		System.out.println("Parent Constructor");
+	}
+
+	static {
+		System.out.println("Parent static block");
+	}
+	{
+		System.out.println("Parent initialisation  block");
+	}
+}
+
+class Child extends Parent {
+	{
+		System.out.println("Child initialisation block");
+	}
+	static {
+		System.out.println("Child static block");
+	}
+
+	public Child() {
+		System.out.println("Child Constructor");
+	}
+
+}
